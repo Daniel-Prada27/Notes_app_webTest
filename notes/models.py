4219bad2ab205bc9384e1f52b9eb0198d18e493f
Daniel <danielspradah0@gmail.com>
2023-11-25 14:26:36 -0500

    first commit

---
+++ notes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=30)
+    note_body = models.CharField(max_length=200)
+    creation_date = models.DateTimeField()
+    def __str__(self):
+        return self.title
+
